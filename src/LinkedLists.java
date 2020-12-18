a74728395366c9ae71c62823153a195f4b6aea00
Micheal Ozdoba <mjozdoba@gmail.com>
2020-12-17 19:21:13 -0800

    Completed 2.9 Loop Detection

--- src/LinkedLists.java
+++ src/LinkedLists.java
@@ -1,9 +1,11 @@
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.Random;
+import java.util.Stack;
 
 public class LinkedLists {
-
     class Node<T> {
         Node<T> next = null;
         T data;
@@ -22,9 +24,15 @@ public class LinkedLists {
         }
 
         public void printLinkedList() {
+            HashSet<Node<T>> hashSet = new HashSet<>();
             Node<T> n = this;
 
             while (n != null) {
+                if (hashSet.contains(n)) {
+                    System.out.println();
+                    return;
+                }
+                hashSet.add(n);
                 System.out.print(n.data + "->");
                 n = n.next;
             }
@@ -37,7 +45,7 @@ public class LinkedLists {
         if (random) {
             Random rand = new Random();
             for (int i = 0; i < size; i++) {
-                head.appendToTail(rand.nextInt(10));
+                head.appendToTail(rand.nextInt(2));
             }
         } else {
             for (int i = 2; i < size; i++) {
@@ -46,8 +54,6 @@ public class LinkedLists {
         }
         return head;
     }
-
-
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////
     //                                        2.1 REMOVE DUPLICATES                                         //
@@ -147,25 +153,27 @@ public class LinkedLists {
         return current;
     }
 
-    class Index {
-        public int value = 0;
+    class Bag {
+        public int index = 0;
+        public int length = 0;
+        public boolean isOdd = false;
     }
 
     public Node<Integer> getKthToLastNodeRecursiveDriver(Node<Integer> head, int k) {
-        Index index = new Index();
+        Bag index = new Bag();
         return getKthToLastNodeRecursive(head, k, index);
     }
 
-    public Node<Integer> getKthToLastNodeRecursive(Node<Integer> node, int k, Index index) {
+    public Node<Integer> getKthToLastNodeRecursive(Node<Integer> node, int k, Bag index) {
         if (node == null) return null;
         Node<Integer> nodeK = getKthToLastNodeRecursive(node.next, k, index);
-        if (k == index.value) nodeK = node;
+        if (k == index.index) nodeK = node;
 
         // Need to increment index every visit on the way back up.
         // If index++ was in an else statement, it would be stuck at the kth node
         // and nodeK will be updated to current node every time after the kth node
         // on the way back up
-        index.value++;
+        index.index++;
         return nodeK;
     }
 
@@ -272,7 +280,7 @@ public class LinkedLists {
     //      Input: (7-> 1 -> 6) + (5 -> 9 -> 2) ...That is, 617 + 295.                                      //
     //      Result: 9 -> 1 -> 2 ...That is, 912.                                                            //
     //                                                                                                      //
-    //      Time Complexity: O(2N) = O(N)                                                                   //
+    //      Time Complexity: O(2MAX(A, B)) = O(N)                                                           //
     //      Space Complexity: O(N)                                                                          //
     //                                                                                                      //
     //      FOLLOW UP                                                                                       //
@@ -282,7 +290,7 @@ public class LinkedLists {
     //      Input: (6 -> 1 -> 7) + (2 -> 9 -> 5)  ...That is, 617 + 295                                     //
     //      Output: 9 -> 1 -> 2 ...That is, 912.                                                            //
     //                                                                                                      //                                                                             //
-    //      Time Complexity: O(2N) = O(N)                                                                   //
+    //      Time Complexity: O(A + B + MAX(A, B)) = O(N)                                                    //
     //      Space Complexity: O(N)                                                                          //
     //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -391,16 +399,171 @@ public class LinkedLists {
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                        2.6 PALINDROME                                                //
+    //      Implement a function to check if a linked list is a palindrome.                                 //
+    //                                                                                                      //
+    //      Time Complexity: O(N)                                                                           //
+    //      Space Complexity: O(N)                                                                          //
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public boolean isPalindromeRecursiveDriver(Node<Integer> head) {
+        Stack<Integer> stack = new Stack<>();
+        Bag bag = new Bag();
+        return isPalindromeRecursive(head, stack, bag);
+    }
+
+    /**
+     * Recursively navigate to end of list, incrementing index with each recursive call.
+     * At the null node, set the length.
+     * On the way back, push values onto the stack if we are before the midpoint. And pop values
+     * after the midpoint. With Odd numbered lists we don't push or pop at the midpoint.
+     */
+    private boolean isPalindromeRecursive(Node<Integer> node, Stack<Integer> stack, Bag bag) {
+        if (node == null) {
+            bag.length = bag.index;
+            bag.isOdd = bag.length%2 != 0;
+            System.out.println("LENGTH = " + bag.length);
+            return true; // is palindrome if length <= 1;
+        }
+        bag.index++;
+        boolean isPalindrome = isPalindromeRecursive(node.next, stack, bag);
+        bag.index--;
+        if (bag.isOdd && bag.index == bag.length/2) {
+            return isPalindrome;
+        }
+        if (bag.index >= bag.length/2) {
+            stack.push(node.data);
+        } else {
+            if (isPalindrome == false) return isPalindrome;
+            isPalindrome = node.data == stack.pop();
+        }
+        return isPalindrome;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
     //                                        2.7 INTERSECTION                                              //
     //      Given two (singly) linked lists, determine if the two lists intersect. Return the               //
     //      inter­secting node. Note that the intersection is defined based on reference, not value.         //
     //      That is, if the kth node of the first linked list is the exact same node (by reference)         //
     //      as the jth node of the second linked list, then they are intersecting.                          //
     //                                                                                                      //
-    //      Time Complexity: O(N)                                                                           //
-    //      Space Complexity: O(1)                                                                          //
+    //      Time Complexity (BruteForce): O(N^2)                                                            //
+    //      Space Complexity (BruteForce): O(1)                                                             //
+    //                                                                                                      //
+    //      Time Complexity (HashTable): O(A+B) = O(N)                                                      //
+    //      Space Complexity (HashTable): O(A+B) = O(N)                                                     //
     //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    public Node<Integer> getIntersectingNode(Node<Integer> head1, Node<Integer> head2) {
+        Node<Integer> n1 = head1;
+
+        while (n1 != null) {
+            Node<Integer> n2 = head2;
+            while (n2 != null) {
+                if (n1 == n2) {
+                    return n1;
+                }
+                n2 = n2.next;
+            }
+            n1 = n1.next;
+        }
+        return null;
+    }
+
+    public Node<Integer> getIntersectingNodeHashTable(Node<Integer> head1, Node<Integer> head2) {
+        Hashtable<Node<Integer>, Node<Integer>> hashTable = new Hashtable<>();
+        Node<Integer> n1 = head1;
+        Node<Integer> n2 = head2;
+
+        while (n1 != null) {
+            hashTable.put(n1, n1);
+            n1=n1.next;
+        }
+
+        while (n2 != null) {
+            if (hashTable.containsKey(n2)) {
+                return n2;
+            }
+            n2 = n2.next;
+        }
+
+        return null;
+    }
+
+    class IntersectingLists<T> {
+        Node<T> list1 = null;
+        Node<T> list2 = null;
+    }
+
+    public IntersectingLists<Integer> createIntersectingLists() {
+        Node<Integer> headA = new Node<>(33);
+        Node<Integer> nodeA = new Node<Integer>(66);
+        Node<Integer> headB = new Node<Integer>(99);
+        Node<Integer> sharedNode = new Node<>(69);
+        Node<Integer> sharedList = createLinkedList(3, true);
+        sharedNode.next = sharedList;
+        headA.next = nodeA;
+        nodeA.next = sharedNode;
+        headB.next = sharedNode;
+
+        IntersectingLists<Integer> lists = new IntersectingLists<>();
+        lists.list1 = headA;
+        lists.list2 = headB;
+        return lists;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                        2.8 LOOP DETECTION                                            //
+    //      Given a circular linked list, implement an algorithm that returns the node at the               //
+    //      beginning of the loop.                                                                          //
+    //                                                                                                      //
+    //      DEFINITION                                                                                      //
+    //      Circular linked list: A (corrupt) linked list in which a node's next pointer points to          //
+    //      an earlier node, so as to make a loop in the linked list.                                       //
+    //                                                                                                      //
+    //      Example:                                                                                        //
+    //      Input: A -> B -> C -> D -> E -> C [the same C as earlier]                                       //
+    //      Result: C                                                                                       //
+    //                                                                                                      //
+    //      Time Complexity (BruteForce): O(N^2)                                                            //
+    //      Space Complexity (BruteForce): O(1)                                                             //
+    //                                                                                                      //
+    //      Time Complexity (HashTable): O(A+B) = O(N)                                                      //
+    //      Space Complexity (HashTable): O(A+B) = O(N)                                                     //
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Iterates through the list and adds each node to a set.
+     * If we encounter a node that we've visited before, we have a circular list
+     * and we return the node that we've visited before (the beginning of the loop)
+     */
+    public Node<Integer> getCorruptNodeFromCircularList(Node<Integer> head) {
+        Node<Integer> node = head;
+        HashSet<Node<Integer>> hashSet = new HashSet<>();
+
+        while (node != null) {
+            if (hashSet.contains(node)) {
+                node.next = null;
+                return node;
+            }
+            hashSet.add(node);
+            node = node.next;
+        }
+
+        return null;
+    }
+
+    public Node<Integer> createCircularLinkedList(int size) {
+        Node<Integer> head = createLinkedList(size, false);
+        Node<Integer> node = head;
+
+        while (node.next != null) {
+            node = node.next;
+        }
+        node.next = head;
+
+        return head;
+    }
     public static void main(String[] args) throws Exception {
         LinkedLists ll = new LinkedLists();
 
@@ -468,14 +631,38 @@ public class LinkedLists {
         Node<Integer> headQ5C = ll.sumReverseOrderLists(headQ5A, headQ5B);
         headQ5C.printLinkedList();
         System.out.print("\n");
-
-        System.out.println("2.5 FOLLOW UP\nSuppose the digits are stored in forward order. Repeat the above problem.\n");
-        System.out.println("ORIGINAL FORWARD DIGIT LINKED LISTS:");
+        System.out.println("2.5 Follow up.\n Suppose the digits are stored in forward order. Repeat the above problem.\n");
+        System.out.println("FORWARD DIGIT SUM:");
         headQ5A.printLinkedList();
         headQ5B.printLinkedList();
-        System.out.println("Forward DIGIT SUM:");
         Node<Integer> headQ5D = ll.sumLinkedLists(headQ5A, headQ5B);
         headQ5D.printLinkedList();
+        System.out.print("\n");
+
+        System.out.println("2.6 Implement a function to check if a linked list is a palindrome.\n");
+        Node<Integer> headQ6 = ll.createLinkedList(2, true);
+        headQ6.printLinkedList();
+        System.out.println("isPalindrome() = " + ll.isPalindromeRecursiveDriver(headQ6));
         System.out.print("\n\n");
+
+        System.out.println("2.7 You have two numbers represented by a linked list, where each node contains a single digit.\n" +
+        "The digits are stored in reverse order, such that the 1 's digit is at the head of the list.\n" +
+        "Write a function that adds the two numbers and returns the sum as a linked list.\n");
+        IntersectingLists<Integer> intersectingLists = ll.createIntersectingLists();
+        System.out.println("INTERSECTING LINKED LISTS:");
+        intersectingLists.list1.printLinkedList();
+        intersectingLists.list2.printLinkedList();
+        Node<Integer> intersectingNode = ll.getIntersectingNodeHashTable(intersectingLists.list1, intersectingLists.list2);
+        System.out.println("INTERSECTING NODE:");
+        intersectingNode.printLinkedList();
+        System.out.print("\n\n");
+
+        System.out.println("Given a circular linked list, implement an algorithm that returns the node at the\n" +
+        "beginning of the loop.\nDEFINITION\nCircular linked list: A (corrupt) linked list in which a node's next pointer points to\n" +
+        "an earlier node, so as to make a loop in the linked list.\n");
+        Node<Integer> circularList = ll.createCircularLinkedList(4);
+        circularList.printLinkedList();
+        Node<Integer> corruptNode = ll.getCorruptNodeFromCircularList(circularList);
+        corruptNode.printLinkedList();
     }
 }
